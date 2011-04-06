24e5091e256f8e16455dbaed6c3c8ba6e63b32c6
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-04-05 23:03:35 -0400

    PCI fixes

--- drivers/PCI/pci.c
+++ drivers/PCI/pci.c
@@ -1,18 +1,43 @@
-struct pci_bus * list_pci()
+#include "pci.h"
+
+void _pci_init(void)
 {
-	for(int i = 0; i < MAX_PCI; i ++)
+	//Need to memset root to 0
+	pci_root.subordinate = _pci_list( &root );	
+}
+
+uint32_t  _pci_list(struct _pci_bus * bus)
+{
+	uint8_t device, function;
+	uint32_t max = bus->secondary;
+	
+	struct _pci_dev * device;
+	struct _pci_dev ** bus_last = &bus->devices;
+
+	struct _pci_bus * child;
+
+	for(device = 0; device < 0x0f; device_function++)
 	{
-		
+		for(function = 0; function < 0x04; function++)
+		{
+
+			uint16_t vendor_id;
+			if((vendor_id = _pci_config_read_word(bus->number , device , function , 0)) != 0xfff)
+			{
+				_cprintf("%s\n" , vendor_id);				
+			}
+		}
 	}
 }
 
-uint16_t _pci_config_read_word(uint16_t bus , uint16_t slot, uint16_t function , uint16_t offset)
+//Maybe these should be chars?
+uint16_t _pci_config_read_word(uint8_t bus , uint8_t device, uint8_t function , uint8_t offset)
 {
-	uint64_t address;
-	uint64_t lbus = (uint64_t)bus;
-	uint64_t lslot = (uint64_t)slot;
-	uint64_t lfunc = (uint64_t)function;
-	unsigned short ret = 0;
+	uint32_t address;
+	uint32_t lbus = (uint64_t)bus;
+	uint32_t lslot = (uint64_t)device;
+	uint32_t lfunc = (uint64_t)function;
+	uint16_t ret = 0;
 
 
 	//TODO: Remove magic numbers
