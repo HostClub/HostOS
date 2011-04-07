782e940ce8f36e06872d7123e44f659e1857aebc
myqe <myqebaril@gmail.com>
2011-04-07 15:25:47 -0400

    Made the paging.h file

---
+++ mm/paging.h
@@ -0,0 +1,46 @@
+#ifndef PAGING_H_
+#define PAGING_H_
+
+// need includes
+
+//read the readme of the layout of the pages
+typedef struct page {
+  uint32_t pres     : 1;
+  uint32_t rw       : 1;
+  uint32_t user     : 1;
+  uint32_t accsd    : 1;
+  uint32_t dirty    : 1;
+  uint32_t unused   : 7;
+  uint32_t frame    : 20;
+} page_t;
+
+
+typedef struct page_table {
+  page_t p[1024];
+} page_table_t;
+
+typedef struct page_dir {
+  
+  //pointers t pagetables
+  page_table_t *tables[1024];
+
+  //pointers to the page tables, gives phys address
+  uint32_t     phys_tables[1024];
+
+  //physical address of the phys_tables.
+  //TODO:more
+  uint32_t     phys_addr
+} page_dir_t;
+
+//init routine for paging
+void init_paging();
+
+//loads the page directory into the CR3 reg
+void change_page_dir(page_dir_t *new); 
+
+//gets a pointer to the required page
+//TODO:more
+page_t *get_page(uint32_t addr, int create, page_dir_t *dir);
+
+
+//TODO:page fault handler
