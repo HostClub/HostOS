0784a7fdf23bc7f7a1dcc8c4092cff8ba6d49ddd
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-04-08 01:10:53 -0400

    Fixed pci_config_read_word method, changed offset use

--- drivers/PCI/pci.c
+++ drivers/PCI/pci.c
@@ -1,6 +1,10 @@
 #include "pci.h"
 #include "startup.h"
 #include "c_io.h"
+
+int CONFIG_DATA = 0xCFC;
+int CONFIG_ADDRESS = 0xCF8;
+
 void _pci_init(void)
 {
 	//Need to memset root to 0
@@ -22,31 +26,50 @@ uint32_t  _pci_list(struct _pci_bus * bus)
 		for(function = 0; function < 0x04; function++)
 		{
 
-			uint16_t vendor_id;
-			if((vendor_id = _pci_config_read_word(bus->number , device_num , function , 0)) != 0xfff)
+			uint32_t vendor_id = _pci_config_read_word(bus->number , device_num , function , 0);
+
+
+			if(vendor_id != -1)
 			{
-				c_printf("%d\n" , vendor_id);				
+			c_puts("Found a function\n");
+
+
+
+			c_printf("device_num: %d function: %d value:%x\n" , device_num , function , vendor_id);
+	
+			uint32_t status = _pci_config_read_word(bus->number , device_num , function , 0x04);
+
+			c_printf("status %x\n" , status);
+
 			}
+			/*if((vendor_id = _pci_config_read_word(bus->number , device_num , function , 0)) != 0xfff)
+			{
+				c_puts("Found good funtion!\n");
+				c_printf("%d\n" , vendor_id);				
+			}*/
 		}
 	}
 }
 
 //Maybe these should be chars?
-uint16_t _pci_config_read_word(uint8_t bus , uint8_t device, uint8_t function , uint8_t offset)
+uint32_t _pci_config_read_word(uint8_t bus , uint8_t device, uint8_t function , uint8_t offset)
 {
 	uint32_t address;
-	uint32_t lbus = (uint64_t)bus;
-	uint32_t lslot = (uint64_t)device;
-	uint32_t lfunc = (uint64_t)function;
-	uint16_t ret = 0;
+	uint32_t lbus = (uint32_t)bus;
+	uint32_t lslot = (uint32_t)device;
+	uint32_t lfunc = (uint32_t)function;
+	uint32_t loffset = (uint32_t)offset;
+	uint32_t ret = 0;
 
 
 	//TODO: Remove magic numbers
 	address = (uint64_t)((lbus << 16) | (lslot << 11) |
-			(lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
+		(lfunc << 8) | (loffset << 2) | ((uint32_t)0x80000000));
 	/* write out the address */
 	__outl (CONFIG_ADDRESS, address);
 	/* read in the data */
-	ret = (uint16_t)((__inl(CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
+	//ret = (uint16_t)((__inl(CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
+	
+	ret = (uint32_t)(__inl(CONFIG_DATA));
 	return (ret);
 }
