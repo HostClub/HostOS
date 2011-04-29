520c54eaf547aa83cfc822f7b2fc09e66bc59d9b
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-04-29 17:40:36 -0400

    Struct changes

--- include/usb_ehci.h
+++ include/usb_ehci.h
@@ -11,7 +11,7 @@ void _isr_usb_int(int vector , int code);
 
 struct _qtd
 {
-	unsigned next_qtd: 27;
+	/*unsigned next_qtd: 27;
 unsigned :4;
 	  unsigned next_qtd_terminate: 1;
 
@@ -30,47 +30,66 @@ unsigned :4;
 	  unsigned error_counter: 2;
 
 	  unsigned pid_code: 2;
-#define OUT_TOKEN 0x00
-#define IN_TOKEN 0x01
-#define SETUP_TOKEN 0x02
+	  */
 
-	  unsigned status: 8;
+	uint32_t next_qtd;
 
-	  void * buffer_list[5];
+	#define NEXT_QTD_OFFSET 5
+	#define NEXT_QTD_TERMINATE (1 << 0)
+
+	uint32_t alternate_qtd;
+
+	#define ALTERNATE_QTD_OFFSET 5
+	#define ALTERNATE_QTD_TERMINATE (1 << 0)
+
+	uint32_t qtd_token;
+
+	#define DATA_TOGGLE_OFFSET 31
+	#define DATA_TOGGLE_ENABLE (1 << DATA_TOGGLE_OFFSET)
+	#define BYTES_TO_TRANSFER_OFFSET 16
+	#define INTERRUPT_ON_COMPLETE_ENABLE (1 << 15)
+
+	#define PID_CODE_OFFSET 8
+
+	#define OUT_TOKEN (0x00 << PID_CODE_OFFSET)
+	#define IN_TOKEN (0x01 << PID_CODE_OFFSET)
+	#define SETUP_TOKEN (0x02 << PID_CODE_OFFSET)
+
+	#define STATUS_ACTIVE_ENABLE (1 << 7)
+
+	void * buffer_list[5];
 #define BUFFER_POINTER_OFFSET 12
 }__attribute__((__packed__));
 
 struct _qtd_head
 {
-	uint32_t qhlp: 27;
+	/*uint32_t qhlp: 27;
 unsigned:2;
 	 uint8_t queue_head_type: 2;
-#define QUEUE_HEAD_TYPE 0x01
+	*/
+	uint32_t qhlp;
+	#define QHLP_OFFSET 5
+	#define QUEUE_HEAD_TYPE_OFFSET 2
+	#define QUEUE_HEAD_TYPE (0x01 << QUEUE_HEAD_TYPE_OFFSET)
+	#define QHLP_TERMINATE 1
 
-	 uint8_t queue_terminate: 1;
+	uint32_t endpoint_char;
 
-	 uint8_t nak_reload: 3;
-	 uint8_t control_endpoint: 1;
-	 uint16_t max_packet_length: 10;
+	#define MAX_PACKET_LENGTH_OFFSET 16
+	
+	#define EPS_OFFSET 12
+	#define EPS_HIGH_SPEED (0x02 << EPS_OFFSET)
+	
+	#define ENDPOINT_NUMBER_OFFSET 8
 
-	 uint8_t reclamation_head: 1;
-	 uint8_t data_toggle_control: 1;
+	#define DEVICE_ADDRESS_OFFSET 0
+	uint32_t endpoint_cap;
 
-	 uint8_t endpoint_speed: 2;
-#define EPS_HIGH_SPEED 0x02
 
-	 uint8_t endpoint_number: 3;
+	#define CURR_QTD_OFFSET 5
+	uint32_t curr_qtd;
 
-	 uint8_t inactivate_on_next: 1;
-
-	 uint8_t device_address: 6;
-
-	 //change this to bitfields later
-	 uint32_t endpoint_cap;
-
-	 uint32_t curr_qtd: 27;
-
-	 struct _qtd qtd;
+	struct _qtd qtd;
 }__attribute__((__packed__));
 
 struct _ehci_request
@@ -89,6 +108,6 @@ void _alloc_qtd_buffer(struct _qtd * curr_qtd , uint32_t buffer_size);
 
 struct _qtd_head * _create_setup_qtd(int device);
 
-struct _qtd_head * _create_qtd_head(int device , int endpoint);
+struct _qtd_head * _create_qtd_head(uint32_t next , int device , int endpoint);
 
 #endif
