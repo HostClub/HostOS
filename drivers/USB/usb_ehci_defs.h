8ee4fb8526dab22a5314060629cd571aacc9c4d5
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-05-19 08:32:51 -0400

    Removed useless comments

--- drivers/USB/usb_ehci_defs.h
+++ drivers/USB/usb_ehci_defs.h
@@ -184,69 +184,7 @@ uint32_t * _CONFIGFLAG;
 //Non-Spec Globals
 
 uint32_t _N_PORTS;
-//uint32_t _MAX_PACKET_LENGTH = 64;
 
-/*
-struct _qtd
-{
-	unsigned next_qtd: 27;
-	unsigned :4;
-	unsigned next_qtd_terminate: 1;
-	
-	unsigned alternate_qtd: 27;
-	unsigned :4;
-	unsigned alternate_qtd_terminate: 1;
-	
-	unsigned data_toggle: 1;
-
-	unsigned bytes_to_transfer: 14;
-
-	unsigned interrupt_on_complete: 1;
-
-	unsigned current_page: 2;
-	
-	unsigned error_counter: 2;
-
-	unsigned pid_code: 2;
-		unsigned status: 8;
-
-	void * buffer_list[5];
-	#define BUFFER_POINTER_OFFSET 12
-}__attribute__((packed));
-
-struct _qtd_head
-{
-	uint32_t qhlp: 27;
-	unsigned:2;
-	uint8_t queue_head_type: 2;
-	#define QUEUE_HEAD_TYPE 0x01
-
-	uint8_t queue_terminate: 1;
-
-	uint8_t nak_reload: 3;
-	uint8_t control_endpoint: 1;
-	uint16_t max_packet_length: 10;
-
-	uint8_t reclamation_head: 1;
-	uint8_t data_toggle_control: 1;
-
-	uint8_t endpoint_speed: 2;
-	#define EPS_HIGH_SPEED 0x02
-
-	uint8_t endpoint_number: 3;
-
-	uint8_t inactivate_on_next: 1;
-
-	uint8_t device_address: 6;
-
-	//change this to bitfields later
-	uint32_t endpoint_cap;
-
-	uint32_t curr_qtd: 27;
-
-	struct _qtd qtd;
-}__attribute__((packed));
-*/
 
 //Private method decs
 
--- include/usb_ehci.h
+++ include/usb_ehci.h
@@ -9,82 +9,19 @@ void usb_ehci_init(struct _pci_dev * device);
 
 void _isr_usb_int(int vector , int code);
 
-/*struct _qtd
-  {
-  uint32_t next_qtd;
-#define NEXT_QTD_OFFSET 5
-*/
 
 #define QTD_TERMINATE 0x01
-/*
-#define NEXT_QTD_TERMINATE 0x01
 
-uint32_t alternate_qtd;
-#define ALTERNATE_QTD_OFFSET 5
-
-#define ALTERNATE_QTD_TERMINATE 0x01
-
-
-uint32_t qtd_token;
-#define DATA_TOGGLE_OFFSET 31
-#define DATA_TOGGLE_ENABLE (1 << DATA_TOGGLE_OFFSET)
-
-#define BYTES_TO_TRANSFER_OFFSET 16
-
-#define INTERRUPT_ON_COMPLETE_ENABLE (1 << 15)
-
-#define PID_CODE_OFFSET 8
-*/
 #define OUT_TOKEN (0x00)
 #define IN_TOKEN (0x01)
 #define SETUP_TOKEN (0x02)
 
 #define STATUS_ACTIVE_ENABLE (1 << 7)
-/*
-void * buffer_list[10];
-}__attribute__((packed));
 
-struct _qtd_head
-{
-uint32_t qhlp;
-#define QHLP_OFFSET 5
-*/
 #define QUEUE_HEAD_TYPE_OFFSET 1
 
 #define QUEUE_HEAD_TYPE (0x01) << QUEUE_HEAD_TYPE_OFFSET
-/*
-#define QHLP_TERMINATE 1
 
-uint32_t endpoint_char;
-
-#define MAX_PACKET_LENGTH_OFFSET 16
-
-#define EPS_OFFSET 12
-#define EPS_HIGH_SPEED (0x02 << EPS_OFFSET)
-
-#define ENDPOINT_NUMBER_OFFSET 8
-
-#define DEVICE_ADDRESS_OFFSET 0
-uint32_t endpoint_cap;
-#define CURR_QTD_OFFSET 5
-uint32_t curr_qtd;
-
-struct _qtd qtd;
-}__attribute__((packed));
-*/
-
-//TAKEN FROM PRETTYOS
-/*struct usbBulkTransfer
-{
-	uint8_t  SCSIopcode;
-	bool     successfulCommand;
-	bool     successfulDataOUT;
-	bool     successfulDataIN;
-	bool     successfulCSW;
-	uint32_t DataBytesToTransferOUT;
-	uint32_t DataBytesToTransferIN;
-}__attribute__((packed));
-*/
 
 #define BUFFER_SIZE 4096
 #define MAX_BYTES_TRANSFER 5 * BUFFER_SIZE
